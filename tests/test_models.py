5a5109a7e0d4f1ca8f40f028a37f71393589797a
sami jaghouar <sami.jaghouar@hotmail.fr>
2021-10-14 17:54:22 +0200

    add tests resnet baseline

---
+++ tests/test_models.py
@@ -0,0 +1,12 @@
+import torch
+
+from vision_transformers.cifar import _IMAGE_SHAPE
+from vision_transformers.resnet import ResNet
+
+
+def test_resnet_output_shape():
+    model = ResNet(10, pretrained=False)
+
+    output = model(torch.zeros((2, 3, *_IMAGE_SHAPE)))
+
+    assert output.shape == (2, 10)
