a9fc5f719645c8b3026ffb019becf53daa9382ef
sami jaghouar <sami.jaghouar@hotmail.fr>
2021-10-17 21:25:46 +0200

    add progressive image resizing

--- vision_transformers/datasets/cifar.py
+++ vision_transformers/datasets/cifar.py
@@ -1,15 +1,17 @@
 from typing import Callable, Optional, Tuple
 
+import numpy as np
 import pytorch_lightning as pl
 import torch
 import torchvision
 from PIL import Image
+from pytorch_lightning.callbacks import Callback
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 
 _MEAN = [0.485, 0.456, 0.406]
 _STD = [0.229, 0.224, 0.225]
-_IMAGE_SHAPE = (28, 28)
+_IMAGE_SHAPE = (30, 30)
 
 
 def get_transforms(image_shape: Tuple[int, int]) -> Callable:
@@ -44,23 +46,23 @@ class CIFARDataModule(pl.LightningDataModule):
         _VAL_PERCENTAGE = 0.1
 
         try:
-            dataset = CIFAR10(
+            self.dataset = CIFAR10(
                 self.data_path, transform=get_transforms(_IMAGE_SHAPE), train=True
             )
         except RuntimeError:
-            dataset = CIFAR10(
+            self.dataset = CIFAR10(
                 self.data_path,
                 transform=get_transforms(_IMAGE_SHAPE),
                 train=True,
                 download=True,
             )
 
-        val_len = int(_VAL_PERCENTAGE * len(dataset))
+        val_len = int(_VAL_PERCENTAGE * len(self.dataset))
 
         self.train_dataset, self.val_dataset = torch.utils.data.random_split(
-            dataset,
+            self.dataset,
             [
-                len(dataset) - val_len,
+                len(self.dataset) - val_len,
                 val_len,
             ],
             generator=torch.Generator().manual_seed(42),
@@ -94,3 +96,37 @@ class CIFARDataModule(pl.LightningDataModule):
         return DataLoader(
             self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers
         )
+
+
+class ProgressiveImageResizing(Callback):
+    def __init__(
+        self,
+        data_module: pl.LightningDataModule,
+        epoch_final_size: int,
+        n_step: int,
+        init_size: int,
+        final_size: int,
+    ):
+        super().__init__()
+        self.data_module = data_module
+
+        step = epoch_final_size // n_step
+        self.epoch_to_change = np.linspace(
+            1, epoch_final_size, n_step, dtype=int
+        ).tolist()
+        self.new_size = np.linspace(init_size, final_size, n_step, dtype=int).tolist()
+
+        self.epoch = 1
+
+        self._change_size(self.new_size[0])
+
+    def on_train_epoch_end(self, trainer, *args, **kwargs):
+        self.epoch += 1
+
+        if self.epoch in self.epoch_to_change:
+            idx = self.epoch_to_change.index(self.epoch)
+            self._change_size(self.new_size[idx])
+            self.log("size", self.new_size[idx])
+
+    def _change_size(self, size: int):
+        self.data_module.dataset.transform = get_transforms((size, size))
